e2c5fb9f39621f1bfadc36edb9370c1779013905
Nazar_Yaremiiy <nazar.yaremiiy@gmail.com>
2024-01-04 01:35:51 +0200

    Connect react icons, created header component and add icon camera

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import CodeBar from './components/CodeBar/CodeBar';
 import { EditorProvider } from './context/context';
+import styles from './App.module.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <EditorProvider>
       <CodeBar />
+      <div className={styles.main}>
+        <Header />
+      </div>
     </EditorProvider>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import styles from './Header.module.css';
+import {
+  BsFillCameraVideoFill,
+  BsFillCameraVideoOffFill,
+} from 'react-icons/bs';
+
+const Header = () => {
+  const [camera, setCamera] = useState(false);
+  return (
+    <header className={styles.header}>
+      <button className={styles.button} onClick={() => setCamera(!camera)}>
+        {camera ? (
+          <BsFillCameraVideoFill fill='#fff' size={23} />
+        ) : (
+          <BsFillCameraVideoOffFill fill='#fff' size={23} />
+        )}
+      </button>
+    </header>
+  );
+};
+
+export default Header;
